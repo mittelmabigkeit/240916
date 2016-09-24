2690e1b014aeca2e1cb03ebe67916638ece674ea
mittelmabigkeit <comp422012016@mail.ru>
2016-09-24 14:26:18 +0300

    update

--- main.js
+++ main.js
@@ -9,3 +9,9 @@ console.log(triangle.ac());
 
 var gipotenuza=(function(a,b){return (a*a)+(b*b)}(3,4));
 console.log(gipotenuza);
+
+var storona=function cc(aa,bb) {
+	gip=(aa*aa)+(bb*bb);
+	return gip;
+}
+console.log(storona(3,4));
