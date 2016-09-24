67b07b5da59fa707fd8eba2865f33abee79a5a01
mittelmabigkeit <comp422012016@mail.ru>
2016-09-24 14:38:33 +0300

    update

--- main.js
+++ main.js
@@ -2,16 +2,20 @@ var triangle={
 	ab:3,
 	bc:4,
 	ac:function(){
-		return (this.ab*this.ab)+(this.bc*this.bc);
+		sss = (this.ab*this.ab)+(this.bc*this.bc);
+		return Math.sqrt(sss);
 	}
 };
 console.log(triangle.ac());
 
-var gipotenuza=(function(a,b){return (a*a)+(b*b)}(3,4));
+var gipotenuza=(function(a,b){
+	aaa=(a*a)+(b*b);
+	return Math.sqrt(aaa);
+}(3,4));
 console.log(gipotenuza);
 
 var storona=function cc(aa,bb) {
 	gip=(aa*aa)+(bb*bb);
-	return gip;
+	return Math.sqrt(gip);
 }
 console.log(storona(3,4));
