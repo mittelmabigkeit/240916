a88b092d1c83cc6d1148b2e5b8d0e40d15155c2b
mittelmabigkeit <comp422012016@mail.ru>
2016-09-26 11:53:35 +0300

    homework

--- main.js
+++ main.js
@@ -17,6 +17,42 @@ var storona=function cc(aa,bb) {
 }
 console.log(storona(3,4));
 
-//Найти длину окружности R=произвольный сconst PI=3.14
+//Найти длину окружности R=произвольный
+
+var dlina=function pp(r) {
+	vav=2*Math.PI*r;
+	return vav;
+}
+console.log(dlina(4));
+
+var lenght=(function(rad){zaz=2*Math.PI*rad; return zaz;}(3));
+console.log(lenght);
+
+var dl={
+	radius:2,
+	fun:function(){
+		hhh=2*Math.PI*this.radius;
+		return hhh;
+	}
+};
+console.log(dl.fun());
+
 //Найти площадь окружности
-//3 способа
+
+var okr={
+	rr:4,
+	func:function(){
+		ploshad = Math.PI*this.rr*this.rr;
+		return ploshad;
+	}
+};
+console.log(okr.func());
+
+var krug=(function(rrr){plosh=Math.PI*rrr*rrr; return plosh;}(3));
+console.log(krug);
+
+var kruzhok=function ppp(rrrr) {
+	ploshadka=Math.PI*rrrr*rrrr;
+	return ploshadka;
+}
+console.log(kruzhok(2));
